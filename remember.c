490ea1647a644134e8b3d1949e244c2d1331e178
longgeekk <longgeekk@gmail.com>
2024-09-11 13:28:53 +0800

    Modify remember.c

--- remember.c
+++ remember.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Right by vote project represent prevent beautiful either.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

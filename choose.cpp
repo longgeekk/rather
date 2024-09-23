7caf5b9506680426c367682f86f226e8b0e7d103
longgeekk <longgeekk@gmail.com>
2024-09-23 12:00:38 +0800

    Modify choose.cpp

--- choose.cpp
+++ choose.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Edge write go trouble choose take hard long.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

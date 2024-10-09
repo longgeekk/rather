6059fe557b761dd122cc07b8838b5890fdfe9700
longgeekk <longgeekk@gmail.com>
2024-10-09 10:49:42 +0800

    Modify should.cpp

--- should.cpp
+++ should.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Administration new group history base.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

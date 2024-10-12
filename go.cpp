23b9f0ee98c24bb97023ced2deec33c9c63a8d73
longgeekk <longgeekk@gmail.com>
2024-10-12 10:04:33 +0800

    Modify go.cpp

--- go.cpp
+++ go.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Three thing phone.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

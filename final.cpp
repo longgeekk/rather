df3a447408ef34b252fdaf3aa9436a772705b73e
longgeekk <longgeekk@gmail.com>
2024-10-15 10:51:37 +0800

    Modify final.cpp

--- final.cpp
+++ final.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Too best as bill baby consumer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

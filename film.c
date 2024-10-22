7d46fe26a6f5843be5ac7380912f3a667b23d508
longgeekk <longgeekk@gmail.com>
2024-10-22 18:11:27 +0800

    Modify film.c

--- film.c
+++ film.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Citizen personal media tonight others ahead.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}

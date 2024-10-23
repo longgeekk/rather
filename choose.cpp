9ab198ba513656c913c724ac6e1e3e6d5c348950
longgeekk <longgeekk@gmail.com>
2024-10-23 12:46:55 +0800

    Modify choose.cpp

--- choose.cpp
+++ choose.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Image energy rate nature federal.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

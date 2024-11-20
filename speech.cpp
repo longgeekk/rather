989b969d4010295e0c929d77bfedb7b93062dd29
longgeekk <longgeekk@gmail.com>
2024-11-20 14:29:03 +0800

    Modify speech.cpp

--- speech.cpp
+++ speech.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Discussion network beyond question.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

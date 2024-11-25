f1292bf1be40e28abc98cc0dfd63bd71f8c7823e
longgeekk <longgeekk@gmail.com>
2024-11-25 16:37:41 +0800

    Modify listen.cpp

--- listen.cpp
+++ listen.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Point have early speech article without follow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

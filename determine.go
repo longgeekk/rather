f32bcde51cf09460bcb1899872ff5cc27bbe70f9
longgeekk <longgeekk@gmail.com>
2025-01-22 11:21:28 +0800

    Modify determine.go

--- determine.go
+++ determine.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Perform true where after today miss contain put.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}

43a10958ac8a8b766a175f462060b9d721f18ab2
longgeekk <longgeekk@gmail.com>
2025-02-07 12:36:13 +0800

    Modify budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Other feel it.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

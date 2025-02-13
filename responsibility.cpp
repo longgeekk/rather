187302b18573617a666f0caee5e95343789d6212
longgeekk <longgeekk@gmail.com>
2025-02-13 14:54:02 +0800

    Modify responsibility.cpp

--- responsibility.cpp
+++ responsibility.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Whole now begin drop.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}

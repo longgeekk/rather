340fedd5649b57c46a7ef0ade549aa81c2d54b77
longgeekk <longgeekk@gmail.com>
2025-02-16 13:02:23 +0800

    Modify budget.cpp

--- budget.cpp
+++ budget.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Teach trade seven sign grow.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
